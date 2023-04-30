93f7ae6dad9925c4dee9d81260cf3c6fd1dbef55
Casey Spaulding <casey.spaulding@me.com>
2023-04-30 19:39:37 -0400

    Add project files.

---
+++ DataAccessBlazor/Models/PersonModel.cs
@@ -0,0 +1,18 @@
+﻿using SupportLibrary.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessBlazor.Models
+{
+    public class PersonModel : IPersonModel
+    {
+        [Required]
+        public DateTime DateOfBirth { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        [Required]
+        public string? FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        [Required]
+        public string? LastName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ SupportLibrary/Models/PersonModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SupportLibrary.Models
+
+// This is the implementation of the interface IPersonModel
+// This is the class that will be used to pass data between the UI and the DataAccess layer
+// This class is used in the DataAccessBlazor project
+
+{
+    internal class PersonModel : IPersonModel
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+
+    }
+}
